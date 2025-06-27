c7b8ab254cfc8cadf30a5fc745da2c7d2cbc03c3
KiCKTheBucket <kayos156official@gmail.com>
2025-06-27 13:30:03 -0400

    Update tooltouchups.java

--- src/main/java/com/kckarnige/tooltouchups/tooltouchups.java
+++ src/main/java/com/kckarnige/tooltouchups/tooltouchups.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class tooltouchups implements ModInitializer {
 
     public static final String MOD_ID = "tooltouchups";
-    public static final Logger LOGGER = LoggerFactory.getLogger("Tool Touch-Ups");
+    public static final Logger LOGGER = LoggerFactory.getLogger("KiCK's Tool Touch-Ups");
 
     @Override
     public void onInitialize() {
