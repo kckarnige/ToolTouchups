1d0329fa71e08689a16bb215a1aac481eff928ab
KiCKTheBucket <kayos156official@gmail.com>
2025-06-29 19:56:07 -0400

    Cleaned up code a bit

--- src/main/java/com/kckarnige/tooltouchups/ItemRegister.java
+++ src/main/java/com/kckarnige/tooltouchups/ItemRegister.java
@@ -13,40 +13,37 @@ import static com.kckarnige.tooltouchups.ToolMaterials.COPPER;
 
 public class ItemRegister {
     public static final Item DIAMOND_INGOT = registerItem("diamond_ingot", new Item.Settings());
-    public static final Item COPPER_SWORD = registerSwordItem("copper_sword",
-            new Item.Settings(), COPPER, 3.0F, -2.4F);
-    public static final Item COPPER_SHOVEL = registerShovelItem("copper_shovel",
-            new Item.Settings(), COPPER, 1.0F, -3.0F);
-    public static final Item COPPER_PICKAXE = registerPickaxeItem("copper_pickaxe",
-            new Item.Settings(), COPPER, 1.0F, -2.8F);
-    public static final Item COPPER_AXE = registerAxeItem("copper_axe",
-            new Item.Settings(), COPPER, 5.0F, -3.2F);
-    public static final Item COPPER_HOE = registerHoeItem("copper_hoe",
-            new Item.Settings(), COPPER, -1.0F, -1.0F);
+    public static final Item COPPER_SWORD = registerToolItem("copper_sword",
+            new Item.Settings(),"sword", COPPER, 3.0F, -2.4F);
+    public static final Item COPPER_SHOVEL = registerToolItem("copper_shovel",
+            new Item.Settings(),"shovel", COPPER, 1.5F, -3.0F);
+    public static final Item COPPER_PICKAXE = registerToolItem("copper_pickaxe",
+            new Item.Settings(),"pickaxe", COPPER, 1.0F, -2.8F);
+    public static final Item COPPER_AXE = registerToolItem("copper_axe",
+            new Item.Settings(),"axe", COPPER, 7.0F, -3.2F);
+    public static final Item COPPER_HOE = registerToolItem("copper_hoe",
+            new Item.Settings(),"hoe", COPPER, -1.0F, -1.0F);
 
     private static Item registerItem (String id, Item.Settings item) {
         RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, id));
         return Registry.register(Registries.ITEM, key, new Item(item.registryKey(key)));
     }
-    private static Item registerSwordItem (String id, Item.Settings item, ToolMaterial material, float atkDmg, float atkSpeed) {
+    private static Item registerToolItem (String id, Item.Settings item, String ToolType, ToolMaterial material, float atkDmg, float atkSpeed) {
         RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, id));
-        return Registry.register(Registries.ITEM, key, new SwordItem(material, atkDmg, atkSpeed, item.registryKey(key)));
-    }
-    private static Item registerShovelItem (String id, Item.Settings item, ToolMaterial material, float atkDmg, float atkSpeed) {
-        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, id));
-        return Registry.register(Registries.ITEM, key, new ShovelItem(material, atkDmg, atkSpeed, item.registryKey(key)));
-    }
-    private static Item registerPickaxeItem (String id, Item.Settings item, ToolMaterial material, float atkDmg, float atkSpeed) {
-        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, id));
-        return Registry.register(Registries.ITEM, key, new PickaxeItem(material, atkDmg, atkSpeed, item.registryKey(key)));
-    }
-    private static Item registerAxeItem (String id, Item.Settings item, ToolMaterial material, float atkDmg, float atkSpeed) {
-        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, id));
-        return Registry.register(Registries.ITEM, key, new AxeItem(material, atkDmg, atkSpeed, item.registryKey(key)));
-    }
-    private static Item registerHoeItem (String id, Item.Settings item, ToolMaterial material, float atkDmg, float atkSpeed) {
-        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, id));
-        return Registry.register(Registries.ITEM, key, new HoeItem(material, atkDmg, atkSpeed, item.registryKey(key)));
+        return switch (ToolType) {
+            case "sword" ->
+                    Registry.register(Registries.ITEM, key, new SwordItem(material, atkDmg, atkSpeed, item.registryKey(key)));
+            case "shovel" ->
+                    Registry.register(Registries.ITEM, key, new ShovelItem(material, atkDmg, atkSpeed, item.registryKey(key)));
+            case "pickaxe" ->
+                    Registry.register(Registries.ITEM, key, new PickaxeItem(material, atkDmg, atkSpeed, item.registryKey(key)));
+            case "axe" ->
+                    Registry.register(Registries.ITEM, key, new AxeItem(material, atkDmg, atkSpeed, item.registryKey(key)));
+            case "hoe" ->
+                    Registry.register(Registries.ITEM, key, new HoeItem(material, atkDmg, atkSpeed, item.registryKey(key)));
+            default ->
+                    throw new IllegalStateException("Unexpected value: " + ToolType);
+        };
     }
 
 
